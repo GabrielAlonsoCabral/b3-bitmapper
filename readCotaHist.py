fca4b8667bc8dca47d396321bab84f0816c7877f
Gabriel Cabral <gabriel.alonso2589@gmail.com>
2023-11-27 21:54:17 -0300

    feat: initial structure

---
+++ readCotaHist.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("_COTAHIST_A2023.csv", low_memory=False)
+
+df1 = df[['CÓDIGO DE NEGOCIAÇÃO DO PAPEL', 'PREÇO DE ABERTURA DO PAPEL-MERCADO NO PREGÃO']]
+print(df1.head())
+# print(df.head())
---
+++ scripts.sh
@@ -0,0 +1,8 @@
+# GENERATE ACTIVATE VIRTUAL ENV
+# python -m venv ./
+
+#ACTIVATE VIRTUAL ENV
+#source env/bin/activate
+
+#DEACTIVATE VIRTUAL ENV
+#deactivate
